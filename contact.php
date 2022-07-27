4ba960a575158e6407939975cab541f45ad1c273
Seoyul <seoyul.rosaria.choi@gmail.com>
2022-07-27 16:48:22 +0900

    print received messages

--- contact.php
+++ contact.php
@@ -18,5 +18,29 @@
         </form>
 
 <?php
+
+  $sql = "
+    SELECT name, message FROM cave_contact
+    ORDER BY no DESC
+  ";
+  $result = mysqli_query($conn, $sql);
+
+  while($row = mysqli_fetch_array($result))
+  {
+    $name = $row['name'];
+    $text = $row['message'];
+
+?>
+
+        <hr />
+
+        <p><?=$name?></p>
+        <p><?=$text?></p>
+
+<?php
+  }
+?>
+
+<?php
   require_once('footer.php');
 ?>
